8002db03516e8598f24b238b6e52b7b35f038490
Ian Wall <ianwall@u.boisestate.edu>
2026-03-30 11:47:22 -0600

    in class reading ints to bytes

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+FILE_NAME="test.bin"
+
+def to_bin(var):
+    print("Writing var to disk in binary format")
+    with open(FILE_NAME, "wb") as f:
+        f.write(var.to_bytes(2, byteorder='big'))
+
+def from_bin():
+    print("Reading binary file to memory")
+    with open(FILE_NAME) as f:
+        binary = f.read().encode("utf-8")
+        return int.from_bytes(binary, byteorder='big')
+
 def main():
-    print("Hello from cs-network-utils-ipblarg!")
+    to_bin(11)
+    var = from_bin()
+    print(var)
 
 
 if __name__ == "__main__":
